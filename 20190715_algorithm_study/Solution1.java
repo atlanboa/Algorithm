b91e72b36eeda20a830df57553ecc5acdfa9b1db
김상헌 <atlanboa@gmail.com>
2019-07-22 14:52:01 +0000

    브랜치 정리

---
+++ 20190715_algorithm_study/Solution1.java
@@ -0,0 +1,32 @@
+
+public class Solution1 {
+	static int a = 10;
+	public static void main(String[] args) {
+		
+		System.out.println(a);
+//		aaa(10, 20);
+//		aaa(10);
+		doSum(10,20,4,5,6,778);
+		doSum(10,20);
+		doSum(10);
+	}
+	
+//	void aaa(*a) {
+//		
+//	}
+	
+	static void aaa(int aa, int bb) {
+		System.out.println(a);
+		
+	}
+	static void aaa(int aa) {
+		System.out.println(a);
+		
+	}
+	static void doSum(int v, int... a) {
+		System.out.println(a.length);
+		
+	}
+	
+	
+}
---
+++ 20190715_algorithm_study/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.print("dfsfd"+"\n");
+		Scanner sc = new Scanner(System.in);
+
+		int a,b,c;
+		for(int i = 0; i < 3; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			c = a + b;
+			System.out.println( (i+1) + " : " + c);	
+		}
+		
+//		
+		
+	}
+
+}
