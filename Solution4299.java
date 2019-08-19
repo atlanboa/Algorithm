990f712d4369a3099f0bb0d44b858ebae1943b57
김상헌 <atlanboa@gmail.com>
2019-08-19 02:23:28 +0000

    4299. 태혁이의 사랑은 타이밍

---
+++ Solution4299.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution4299 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		for(int t=1; t<=T; t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int d, h, m;
+			d = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			m= Integer.parseInt(st.nextToken());
+			
+			int sum = 0;
+			
+			if(m-11< 0) {
+				h--;
+				sum += m+49;
+			}
+			else {
+				sum += m-11;
+			}
+			
+			if(h-11 < 0) {
+				d--;
+				sum += (h+13)*60;
+			}else {
+				sum += 60*(h-11);
+			}
+			
+			if(d < 11) {
+				sum = -1;
+			}else {
+				sum += (d-11)*1440;
+			}
+		
+			System.out.println("#"+t+" "+sum);
+			
+		}
+
+	}
+
+}
