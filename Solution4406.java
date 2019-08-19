96e52b187a55296b3f307ee989924e8c3dda356f
김상헌 <atlanboa@gmail.com>
2019-08-19 00:40:47 +0000

    4406. 모음이 보이지 않는 사람

---
+++ Solution4406.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution4406 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t=1; t<=T;t++) {
+			
+			
+			
+			String str = br.readLine();
+			
+			System.out.print("#"+t+" ");
+			
+			for(char a:str.toCharArray()) {
+				switch(a){
+					case 'a':
+					case 'e':
+					case 'i':
+					case 'o':
+					case 'u':
+						continue;
+				}
+				System.out.print(a);
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
