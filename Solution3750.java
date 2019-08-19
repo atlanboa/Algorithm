e5a5950f51f72d60c4d78a127ec1f9aa6c4a9ed5
김상헌 <atlanboa@gmail.com>
2019-08-19 06:25:07 +0000

    3750. Digit sum

---
+++ Solution3750.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution3750 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t=1; t<=T; t++) {
+			String n = br.readLine();
+			
+			while(!(n.length()==1)) {
+				n = f(n);
+			}
+			
+			System.out.println("#"+t+" "+n);
+		}
+	}
+
+	private static String f(String n) {
+		int sum = 0;
+		for(char a: n.toCharArray()) {
+			sum += a-'0';
+		}
+		
+		
+		return Integer.toString(sum);
+	}
+
+}
