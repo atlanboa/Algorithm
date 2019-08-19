8eb741ef3a96a27ca8935bd5ea9a3db1af5a293b
김상헌 <atlanboa@gmail.com>
2019-08-19 06:10:37 +0000

    3809. 화섭이의 정수 나열

---
+++ Solution3809.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution3809 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		Scanner sc = new Scanner(System.in);
+//		int T = Integer.parseInt(br.readLine());
+		int T = sc.nextInt();
+		for(int t=1; t<=T; t++) {
+			boolean[] chk = new boolean[1000];
+//			int n = Integer.parseInt(br.readLine());
+			int n = sc.nextInt();
+			
+//			st = new StringTokenizer(br.readLine());
+			
+			String str = "";
+			for(int i=0; i<n; i++) {
+				str += sc.nextInt();
+				
+			}
+			
+			
+			int idx = 0;
+			while(true) {
+				if(!str.contains(Integer.toString(idx))) {
+					break;
+				}else {
+					idx++;
+				}
+			}
+			
+			System.out.println("#"+t+" "+idx);
+
+			
+		}
+
+	}
+
+
+
+}
