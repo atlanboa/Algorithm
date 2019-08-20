554d749ea6284253356936383411d4b1f076c07f
김상헌 <atlanboa@gmail.com>
2019-08-20 07:17:01 +0000

    3131. 100만 이하의 모든 소수 에라토스테네스의 체

---
+++ Solution3131.java
@@ -0,0 +1,24 @@
+
+public class Solution3131 {
+	
+	public static void main(String[] args) {
+		boolean[] prime = new boolean[1000001];
+		
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i=2; i<Math.sqrt(1000000); i++) {
+			for(int j=i*i; j<prime.length; j+=i) {
+				prime[j] = true;
+			}
+		}
+		
+		
+		for(int i=0; i<prime.length; i++) {
+			if(!prime[i]) System.out.print(i+" ");
+		}
+		
+
+	}
+
+}
