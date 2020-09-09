1f7b530513fcc9debcfb6df71b980c90760561d0
atlanboa <atlanboa@gmail.com>
2020-09-09 21:42:41 +0900

    [카카오 2019] 후보키 with java
    비트마스크로 부분 집합 생성시 특이점에 대해서 알 수 있는 문제였음

---
+++ Kakao_후보키.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Kakao_후보키 {
+    public int solution(String[][] relation) {
+        int answer;
+
+        int attribute = relation[0].length;
+        int tuple = relation.length;
+        HashSet<String> set = new HashSet<>();
+        HashSet<Integer> candidateKey = new HashSet<>();
+
+        for(int i=1; i<(1<<attribute); i++){
+            set.clear();
+
+            for(int j=0; j<tuple; j++){
+                StringBuilder key = new StringBuilder();
+                for(int k=0; k<attribute; k++){
+                    if( (i & (1<<k)) != 0){
+                        key.append(relation[j][k]);
+                    }
+                }
+//                System.out.println(key.toString());
+                set.add(key.toString());
+            }
+
+            if(set.size() == tuple) pushOnlyMinimality(i, candidateKey);
+        }
+
+        answer = candidateKey.size();
+//        System.out.println(answer);
+
+        return answer;
+    }
+
+    private void pushOnlyMinimality(int newKey, HashSet<Integer> candidateKey) {
+        for(int candidate: candidateKey){
+            if( (candidate & newKey) == candidate){
+                return;
+            }
+        }
+        candidateKey.add(newKey);
+    }
+
+
+    public static void main(String[] args) {
+        String[][] relation = 
+                {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+        Kakao_후보키 temp = new Kakao_후보키();
+        temp.solution(relation);
+
+    }
+}
